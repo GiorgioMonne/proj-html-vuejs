682bd1edb4ceec62aefe312a92fce6f5f64f9a60
Giorgio <1999giorgio.monne@mail.com>
2022-01-13 11:34:14 -1000

    continuio

---
+++ src/components/Header.vue
@@ -0,0 +1,136 @@
+<template>
+  <header>
+      <div class="nav">
+          <ul>
+              <li class="left">
+                  <a href="#">dfgdf</a>
+                  <a href="#">dfgg</a>
+                  <a href="#">dfg</a>
+              </li>
+              <li class="titolo">
+                <h1>AVADA SPA</h1>
+              </li>
+              <li class="right">
+                  <a href="#">dfg</a>
+                  <a href="#">fgsdfg</a>
+                  <a href="#">dgdg</a>
+                  <a href="#">+</a>
+              </li>
+          </ul>
+      </div>
+
+      <div class="jumbo">
+          <img src="../assets/foto/avada-spa-slider-image-1-new.jpg" alt="">
+
+          <div class="cubo-info">
+              <div class="cubo-uno">
+                  <img src="" alt="">
+                  <h5>Demos</h5>
+              </div>
+
+              <div class="cubo-due">
+                  <h3>£39</h3>
+                  <h4>On Sale</h4>
+              </div>
+          </div>
+
+          <div class="info">
+              <h4>Give Yuorself permission</h4>
+              <h2>Relax, Revive & Rejuvenate</h2>
+              <button>EXPLORE TREATMENTS</button>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang='scss' scoped>
+
+    .nav{
+        height: 80px;
+        width: 100%;
+        background-color: greenyellow;
+
+        ul{
+            display: flex;
+            justify-content: center;
+            line-height: 80px;
+
+            li{                
+                list-style: none;
+                margin: 0 30px;
+
+                a{
+                    margin: 20px;
+                }
+            }
+        }
+    }
+
+    .jumbo{
+        height: 850px;
+        width: 100%;
+        position: relative;
+        background-color: bisque;
+
+        img{
+            height: 100%;
+            width: 100%;
+        }
+
+        .cubo-uno{
+            position: absolute;
+            top: 40px;
+            right: 10px;
+            height: 80px;
+            width: 80px;
+            border-radius: 10px;
+            background-color: pink;
+            text-align: center;
+        }
+
+        .cubo-due{
+            position: absolute;
+            top: 140px;
+            right: 10px;
+            height: 80px;
+            width: 80px;
+            border-radius: 10px;
+            background-color: pink;
+            text-align: center;
+        }
+
+        .info{
+            position: absolute;
+            text-align: center;
+            color: white;
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%, -40%);
+
+            h4{
+                font-size: 30px;
+            }
+
+            h2{
+                font-size: 70px;
+            }
+
+            button{
+                margin: 30px;
+                padding: 10px 20px;
+                border: none;
+                border-radius: 15px;
+                color: white;
+                font-weight: bold;
+                background-color: rgb(206, 203, 203);
+            }
+        }
+    }
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+      <div class="scriptum">
+          <h2>Luxury Spa Experiences</h2>
+          <i class="fab fa-envira"></i>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
